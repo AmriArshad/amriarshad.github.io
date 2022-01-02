63b64807c0b5ef3a89397bf53d0e7bfd6efde167
Amri <amri.arshad16@yahoo.com>
2022-01-02 15:08:07 +1300

    added Hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+      ☪️ | Computer Science Student | Digital Artist | Creative Coder
+      </SectionText>
+      <Button onClick={() => window.location = 'https://linktr.ee/TheMel0n'}>Learn more</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
