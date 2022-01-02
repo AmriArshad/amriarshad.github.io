66c125e6e3e2e9a9594a36da77a7a6099b7c771c
Amri <amri.arshad16@yahoo.com>
2022-01-02 16:37:02 +1300

    added template accomplishments section

---
+++ src/components/Accomplishments/Accomplishments.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
+import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
+import { accomplishmentsData } from '../../constants/constants';
+
+const Accomplishments = () => (
+  <Section>
+    <SectionTitle>Personal Achievements</SectionTitle>
+    <Boxes>
+      {accomplishmentsData.map((card, index) => (
+        <Box key={index}>
+          <BoxNum>{card.number}</BoxNum>
+          <BoxText>{card.text}</BoxText>
+        </Box>
+      ))}
+    </Boxes>
+  </Section>
+);
+
+export default Accomplishments;
